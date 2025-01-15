7e6f562fcfd1bb5bb8abb0ae46de0dd706bcfe82
Cai Yuanrui <caiyuanrui@caiyuanruideMacBook-Air.local>
2025-01-15 13:22:30 +0800

    Add health_check service

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
-use axum::{extract, routing::get, Router};
+use axum::{extract, http::StatusCode, response::IntoResponse, routing::get, Router};
+
+type Error = Box<dyn std::error::Error + Send + Sync>;
 
 async fn greeter(path: Option<extract::Path<String>>) -> String {
     format!("Hello {}!", path.map(|e| e.0).unwrap_or("World".into()))
 }
 
-type Error = Box<dyn std::error::Error + Send + Sync>;
+async fn health_check() -> impl IntoResponse {
+    StatusCode::OK
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
@@ -12,9 +16,10 @@ async fn main() -> Result<(), Error> {
 
     let app = Router::new()
         .route("/", get(greeter))
-        .route("/{name}", get(greeter));
+        .route("/{name}", get(greeter))
+        .route("/health_check", get(health_check));
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await?;
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8000").await?;
     axum::serve(listener, app).await?;
     Ok(())
 }
