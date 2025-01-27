d14c6c02a0931ebc0fc733359ebdf75b4e6338f4
Cai Yuanrui <caiyuanrui@outlook.com>
2025-01-27 15:58:31 +0800

    Verify hmac tag in /login GET endpoint

--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -1,15 +1,31 @@
+use anyhow::Context;
 use axum::{
-    extract::Query,
+    extract::{Query, State},
     response::{Html, IntoResponse},
 };
+use hmac::{Hmac, Mac};
 use hyper::StatusCode;
+use secrecy::ExposeSecret;
 use serde::Deserialize;
+use sha3::Sha3_256;
 
-pub async fn login_form(Query(query): Query<QueryParams>) -> impl IntoResponse {
-    let error_html = query
-        .error
-        .map(|c| format!("<p><i>{}</i></p>", htmlescape::encode_minimal(&c)))
-        .unwrap_or("".into());
+use crate::appstate::{AppState, HmacSecret};
+
+pub async fn login_form(
+    Query(query): Query<QueryParams>,
+    State(shared_state): State<AppState>,
+) -> impl IntoResponse {
+    let error_html = match query.verify(&shared_state.hmac_secret) {
+        Ok(error_message) => format!(
+            "<p><i>{}</i></p>",
+            htmlescape::encode_minimal(error_message.as_str())
+        ),
+        Err(e) => {
+            tracing::warn!(error.message = %e, error.cause_chain = ?e, "Failed to verify query parameters using the HMAC tag");
+            "".into()
+        }
+    };
+
     (
         StatusCode::OK,
         Html::from(format!(
@@ -51,4 +67,19 @@ pub async fn login_form(Query(query): Query<QueryParams>) -> impl IntoResponse {
 #[derive(Debug, Deserialize)]
 pub struct QueryParams {
     pub error: Option<String>,
+    pub tag: Option<String>,
+}
+
+impl QueryParams {
+    fn verify(mut self, secret: &HmacSecret) -> Result<String, anyhow::Error> {
+        let tag = hex::decode(self.tag.take().context("tag is missing")?)?;
+        let error = self.error.take().context("error message is missing")?;
+        let error_message = urlencoding::encode(error.as_str());
+
+        let mut mac = Hmac::<Sha3_256>::new_from_slice(secret.expose_secret().as_bytes())?;
+        mac.update(error_message.as_bytes());
+        mac.verify_slice(&tag)?;
+
+        Ok(error)
+    }
 }
