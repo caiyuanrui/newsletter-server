c4705a4311f423f7bce02ebbe0494d4d3c101682
Cai Yuanrui <caiyuanrui@outlook.com>
2025-02-02 22:17:17 +0800

    Refactoring: reject anonymous user

--- src/routes/admin/password/post.rs
+++ src/routes/admin/password/post.rs
@@ -12,6 +12,7 @@ use uuid::Uuid;
 
 use crate::{
     authentication::{self, validate_credentials, AuthError, Credentials},
+    domain::SubscriberId,
     routes::admin::dashboard::get_username,
     session_state::TypedSession,
     utils::{e500, see_other},
@@ -23,64 +24,70 @@ pub async fn change_password(
     messages: Messages,
     Form(form): Form<FormData>,
 ) -> Result<Response, Response> {
-    match session.get_user_id().await.map_err(e500)? {
-        None => Ok(see_other("/login")),
-        Some(user_id) => {
-            // Reject: Two different new passwords
-            if form.new_password.expose_secret() != form.new_password_check.expose_secret() {
-                messages.error(
-                    "You entered two different new passwords - the field values must match.",
-                );
+    let user_id = reject_anonymous_user(session, messages.clone()).await?;
 
-                return Ok((
-                    StatusCode::SEE_OTHER,
-                    [(header::LOCATION, "/admin/password")],
-                )
-                    .into_response());
-            }
+    // Reject: Two different new passwords
+    if form.new_password.expose_secret() != form.new_password_check.expose_secret() {
+        messages.error("You entered two different new passwords - the field values must match.");
 
-            // Reject: The new password is too simple or too long
-            let password_length = form.new_password.expose_secret().len();
-            if password_length <= 12 {
-                messages.error("The new password is too short.");
-                return Ok((
-                    StatusCode::SEE_OTHER,
-                    [(header::LOCATION, "/admin/password")],
-                )
-                    .into_response());
-            }
-            if password_length >= 128 {
-                messages.error("The new password is too long.");
-                return Ok((
-                    StatusCode::SEE_OTHER,
-                    [(header::LOCATION, "/admin/password")],
-                )
-                    .into_response());
-            }
-
-            let username = get_username(user_id, &pool).await.map_err(e500)?;
-
-            let credentials = Credentials {
-                username,
-                password: form.current_password,
-            };
-
-            match validate_credentials(credentials, &pool).await {
-                Ok(user_id) => {
-                    authentication::change_password(user_id, form.new_password, &pool)
-                        .await
-                        .map_err(e500)?;
-                    messages.error("Your password has been changed.");
-                    Ok(see_other("/admin/password"))
-                }
-                Err(AuthError::InvalidCredentials(_)) => {
-                    messages.error("The current password is incorrect.");
-                    Ok(see_other("/admin/password"))
-                }
-                Err(e @ AuthError::UnexpectedError(_)) => Err(e500(e)),
-            }
-        }
+        return Ok((
+            StatusCode::SEE_OTHER,
+            [(header::LOCATION, "/admin/password")],
+        )
+            .into_response());
     }
+
+    // Reject: The new password is too simple or too long
+    let password_length = form.new_password.expose_secret().len();
+    if password_length <= 12 {
+        messages.error("The new password is too short.");
+        return Ok((
+            StatusCode::SEE_OTHER,
+            [(header::LOCATION, "/admin/password")],
+        )
+            .into_response());
+    }
+    if password_length >= 128 {
+        messages.error("The new password is too long.");
+        return Ok((
+            StatusCode::SEE_OTHER,
+            [(header::LOCATION, "/admin/password")],
+        )
+            .into_response());
+    }
+
+    let username = get_username(user_id, &pool).await.map_err(e500)?;
+
+    let credentials = Credentials {
+        username,
+        password: form.current_password,
+    };
+
+    match validate_credentials(credentials, &pool).await {
+        Ok(user_id) => {
+            authentication::change_password(user_id, form.new_password, &pool)
+                .await
+                .map_err(e500)?;
+            messages.error("Your password has been changed.");
+            Ok(see_other("/admin/password"))
+        }
+        Err(AuthError::InvalidCredentials(_)) => {
+            messages.error("The current password is incorrect.");
+            Ok(see_other("/admin/password"))
+        }
+        Err(e @ AuthError::UnexpectedError(_)) => Err(e500(e)),
+    }
+}
+
+async fn reject_anonymous_user(
+    session: TypedSession,
+    messages: Messages,
+) -> Result<SubscriberId, Response> {
+    session.get_user_id().await.map_err(e500)?.ok_or_else(|| {
+        let e = anyhow::anyhow!("The user has not logged in");
+        messages.error(e.to_string());
+        (StatusCode::SEE_OTHER, [(header::LOCATION, "/login")]).into_response()
+    })
 }
 
 #[derive(Debug, serde::Deserialize)]
